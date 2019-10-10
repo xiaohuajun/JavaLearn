db8c1554202ecf89f5b825d0fda21a502ec1dc71
xiaohuajun <18061741650@163.com>
2019-10-10 11:20:55 +0800

    增加了日志配置文件的注释

---
+++ src/test/java/BasicTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+
+
+/**
+ * @author Danny.
+ * @version 1.0
+ * @date 2019/10/9 09:50
+ * @description
+ */
+public class BasicTest {
+
+
+    @Test
+    public void sTest() {
+
+
+    }
+}
